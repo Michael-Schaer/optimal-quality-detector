26b4221dd5ea9adf3850ec76d9ccb002caabafb5
Michael-Schaer <mdschaer@gmail.com>
2016-02-22 17:12:34 +0100

    Added HowTo Images

--- Assets/QualityLevelDetector_QLD/Scripts/BenchmarkController.cs
+++ Assets/QualityLevelDetector_QLD/Scripts/BenchmarkController.cs
@@ -6,7 +6,6 @@ using System;
 [RequireComponent(typeof(FPSCounter))]
 public class BenchmarkController : MonoBehaviour
 {
-    public float fpsDropMaxCount = 10;
     public float timeLimit_sec = 4;
 
     private FPSCounter fpsCounter;
