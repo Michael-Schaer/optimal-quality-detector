a7ef171ee1f7f2fb542eb2b1207ec65db9a36cfb
Michael-Schaer <mdschaer@gmail.com>
2016-02-23 01:21:45 +0100

    CPU vs. GPU Loaders fix

---
+++ Assets/QualityLevelDetector_QLD/Scripts/SceneTools/CPUStresser.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CPUStresser : MonoBehaviour
+{
+    public int loopsPerFrame = 1000;
+
+	void Update ()
+    {
+        int i = 0;
+        while (i < loopsPerFrame)
+        {
+            Vector3 vector = new Vector3(Random.value, Random.value, Random.value);
+            vector.Normalize();
+            i += Mathf.RoundToInt(vector.magnitude);
+        }
+	}
+}
